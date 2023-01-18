023c8f17465f52a5df7fa720b88da784a2c5a08e
Christian Bingman <christian@christianbingman.com>
2023-01-17 22:17:32 -0600

    Disabled pointless errors

--- buildtest.sh
+++ buildtest.sh
@@ -6,6 +6,6 @@ set -o nounset
 set -o pipefail
 
 docker build -t localcs450:latest .
-cd src && docker run --rm -it localcs450:latest -m pycodestyle ./*.py
-docker run --rm -it localcs450:latest -m pylint --recursive=y .
+cd src && docker run --rm -it localcs450:latest -m pycodestyle --ignore=E501 ./*.py
+docker run --rm -it localcs450:latest -m pylint --disable=line-too-long --recursive=y .
 docker run --rm -it localcs450:latest
